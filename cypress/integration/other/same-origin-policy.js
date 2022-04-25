46238ef25ec8cc3ba778661f7aba1ce492e1011b
geonnia <oksana.wonderland@gmail.com>
2022-04-25 14:02:23 +0200

    added cypress dashboard test trigger command

--- cypress/integration/other/same-origin-policy.js
+++ cypress/integration/other/same-origin-policy.js
@@ -2,13 +2,13 @@
 
 describe('Cypress web security', () => {
     it('VALIDATE VISITING TWO DIFFERENT DOMAINS ', () =>{
-        cy.visit('https://webdriveruniversity.com')
-        cy.visit('https://automationteststore.com/')
+        // cy.visit('https://webdriveruniversity.com')
+        // cy.visit('https://automationteststore.com/')
     });
     //worked with chromeWebSecurity: false
     it('VALIDATE VISITING TWO DIFFERENT DOMAINS via user actions', () =>{
         //cy.visit('https://automationteststore.com/')
-        cy.visit('https://webdriveruniversity.com')
-        cy.get('#automation-test-store').invoke('removeAttr', 'target').click()
+        // cy.visit('https://webdriveruniversity.com')
+        // cy.get('#automation-test-store').invoke('removeAttr', 'target').click()
     });
 })
--- cypress/integration/webdriver-uni/select-dropdown-list.js
+++ cypress/integration/webdriver-uni/select-dropdown-list.js
@@ -1,15 +1,15 @@
 /// <reference types = "cypress" />
 
 describe('Interact with dropdown lists via webdriveruni', () => {
-    it.only('Select specific values via selectdropdown lists', () => {
+    it.only('Select specific values via select dropdown lists', () => {
         cy.visit('https://webdriveruniversity.com')
-        cy.get('#dropdown-checkboxes-radiobuttons').invoke('removeAttr', 'target').click()
-        cy.get('#dropdowm-menu-1').select('c#')
-        cy.get('#dropdowm-menu-2').select('testng').should('have.value', 'testng')
-        cy.get('#dropdowm-menu-3').select('jquery').contains('JQuery')
+        cy.get('#dropdown-checkboxes-radio buttons').invoke('removeAttr', 'target').click()
+        cy.get('#dropdown-menu-1').select('c#')
+        cy.get('#dropdown-menu-2').select('testng').should('have.value', 'testng')
+        cy.get('#dropdown-menu-3').select('jquery').contains('JQuery')
 
-        cy.get('#dropdowm-menu-2').select('maven').should('have.value', 'maven')
-        cy.get('#dropdowm-menu-2').select('Maven').contains('Maven')
+        cy.get('#dropdown-menu-2').select('maven').should('have.value', 'maven')
+        cy.get('#dropdown-menu-2').select('Maven').contains('Maven')
        
     });
 
